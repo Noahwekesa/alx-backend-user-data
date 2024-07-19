e4d17a71206ab3989795269d2d7aec37e66820b7
noahwekesa <noahwekesa0@gmail.com>
2024-07-19 22:25:37 +0300

    sesssion

--- 0x02-Session_authentication/0x01-Basic_authentication/api/v1/auth/session_auth.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""This module contains the session authentication logic"""
-from api.v1.auth.auth import Auth
-from models.user import User
-from typing import TypeVar
-from flask import request
-from os import getenv()
